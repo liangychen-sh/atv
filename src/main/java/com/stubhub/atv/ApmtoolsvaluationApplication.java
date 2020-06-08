8021ed7f1657a5a663206f5974aa913ff87c8d7e
Ivan chen <liangychen@stubhub.com>
2020-06-08 16:55:36 +0800

    1. create base repository

---
+++ src/main/java/com/stubhub/atv/ApmtoolsvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.stubhub.atv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmtoolsvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmtoolsvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stubhub/atv/ApmtoolsvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stubhub.atv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmtoolsvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
