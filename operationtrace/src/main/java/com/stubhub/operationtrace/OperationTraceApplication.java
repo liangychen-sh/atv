e5ed19c5bb7de807bc1a3654b1667ac27e59928f
liangychen <liangychen@stubhub.com>
2020-07-10 16:10:27 +0800

    add Test some log

--- operationtrace/src/main/java/com/stubhub/operationtrace/OperationTraceApplication.java
+++ operationtrace/src/main/java/com/stubhub/operationtrace/OperationTraceApplication.java
@@ -3,6 +3,8 @@ package com.stubhub.operationtrace;
 import com.google.auth.Credentials;
 import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
 import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +19,8 @@ import java.util.Map;
 @SpringBootApplication
 public class OperationTraceApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OperationTraceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(OperationTraceApplication.class);
     }
@@ -24,7 +28,10 @@ public class OperationTraceApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
+            LOGGER.info("OperationTraceApplication Started");
+            LOGGER.info("Starting create and register stackdriver");
             StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.builder().build());
+            LOGGER.info("Create and Register stackdriver exporter successfully");
         };
     }
 
